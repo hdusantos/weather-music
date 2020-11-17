ab019720992e7cfbfa3c1efa7b7f307dc496ca1c
Hudson dos Santos <hudson.alves@live.com>
2020-11-17 20:32:16 -0300

    Add environments to spotify API

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,10 +6,18 @@ const environment = {
 
 const openWeatherMap = {
     baseUrl: process.env.OPEN_WEATHER_MAP_API_URL || 'http://api.openweathermap.org/data/2.5/weather',
-    token: process.env.OPEN_WEATHER_MAP_API_TOKEN || 'myToken',
+    token: process.env.OPEN_WEATHER_MAP_API_TOKEN || 'myTokenOpenWeatherMap',
+};
+
+const spotify = {
+    baseUrl: process.env.SPOTIFY_API_URL || '',
+    authUrl: process.env.SPOTIFY_AUTH_URL || 'https://accounts.spotify.com/api/token',
+    clientId: process.env.SPOTIFY_API_CLIENT_ID || 'spotify_client_id',
+    clientSecret: process.env.SPOTIFY_API_CLIENT_SECRET || 'spotify_client_secret',
 };
 
 export {
     environment,
     openWeatherMap,
+    spotify,
 };
