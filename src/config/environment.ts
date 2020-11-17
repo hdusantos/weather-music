5c1273b8bda64ccbb46968cda3ba332945f7aab4
Hudson dos Santos <hudson.alves@live.com>
2020-11-17 16:32:07 -0300

    Add OpenweatherMap environment

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,6 +4,12 @@ const environment = {
     serverPort: process.env.SERVER_PORT || '3001',
 };
 
+const openWeatherMap = {
+    baseUrl: process.env.OPEN_WEATHER_MAP_API_URL || 'http://api.openweathermap.org/data/2.5/weather',
+    token: process.env.OPEN_WEATHER_MAP_API_TOKEN || 'myToken',
+};
+
 export {
     environment,
+    openWeatherMap,
 };
