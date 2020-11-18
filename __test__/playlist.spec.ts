1f25070c554313f0f22d769b7015ebc928dc213f
Hudson dos Santos <hudson.alves@live.com>
2020-11-18 15:39:29 -0300

    Test: latitude, longitude and error handle

--- __test__/playlist.spec.ts
+++ __test__/playlist.spec.ts
@@ -1,7 +1,7 @@
 import Playlist from '../src/services/playlist/playlist';
 import { Location } from '../src/types/types';
 
-test('Tracks must return items', async () => {
+test('Using city name: The return should contain items', async () => {
     const city: string = 'campinas';
     const location: Location = { city };
 
@@ -10,4 +10,37 @@ test('Tracks must return items', async () => {
 
     expect(tracks).toBeDefined();
     expect(tracks).toHaveProperty('items');
+});
+
+test('Using lat and lon: The return should contain items', async () => {
+    const location: Location = {
+        lat: '55.751244',
+        lon: '37.618423',
+    };
+
+    const playlist = new Playlist(location);
+    const tracks = await playlist.getTracks();
+
+    expect(tracks).toBeDefined();
+    expect(tracks).toHaveProperty('items');
+});
+
+test('Using invalid city name: Should throw an exception', async () => {
+    const city: string = 'aaaaaa';
+    const location: Location = { city };
+
+    const playlist = new Playlist(location);
+
+    await expect(playlist.getTracks()).rejects.toThrowError();
+});
+
+test('Using invalid lat and lon: Should throw an exception', async () => {
+    const location: Location = {
+        lat: '1111111111111111',
+        lon: '2222222222222222',
+    };
+
+    const playlist = new Playlist(location);
+
+    await expect(playlist.getTracks()).rejects.toThrowError();
 });
